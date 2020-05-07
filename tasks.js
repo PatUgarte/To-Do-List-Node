d3b8360866361ca6cd250a70f580a48bbc2cb83a
Pat Ugarte <patriciomugarte@gmail.com>
2020-05-07 11:03:17 -0300

    Agregada la comprobacion de si existe el JSON, algunos try-catch

--- tasks.js
+++ tasks.js
@@ -2,21 +2,34 @@ const fs = require('fs');
 
 const jsonPath = "./tareas.json";
 
+if(! fs.existsSync(jsonPath)){
+    try {
+        fs.appendFileSync(jsonPath, '[]');
+        console.log(` >>>   Creado un nuevo archivo de tareas.json vacío    <<< `);
+    } catch (err) {
+        console.error(err);
+    }
+}
+
 let tasksArray = readJSON(jsonPath);
 
 const tasks = {
-    file : "./tareas.json",
-    list : null,
-    search : (value) => {
+    file: "./tareas.json",
+    list: null,
+    search: (value) => {
         console.log(`RESULTADOS DE LA BÚSQUEDA DE "${value}":`);
         return tasksArray.filter((task) => task.estado.toLowerCase().includes(value.toLowerCase()));
     },
-    delete : null,
+    delete: null,
 }
 
 module.exports = tasks;
 
 function readJSON(path) {
-    let string = fs.readFileSync(path, { encoding: "utf-8" });
-    return JSON.parse(string);
+    try {
+        let string = fs.readFileSync(path, { encoding: "utf-8" });
+        return JSON.parse(string);
+    } catch(error){
+        console.error(error);
+    }
 }
