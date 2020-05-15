1f22b98bef7e7012413af0aaa97f905d27e5a2b3
Pat Ugarte <patriciomugarte@gmail.com>
2020-05-14 23:53:02 -0300

    Funcionalidad agregar finalizada, busqueda mejorada ahora por criterio, listar mejorada

--- app.js
+++ app.js
@@ -1,38 +1,74 @@
+const fs = require('fs');
 const tasks = require("./tasks");
 
-const option = cleanInput(process.argv[2]);
-const value = process.argv[3];
+if (!fs.existsSync(tasks.file)) {
+    try {
+        fs.appendFileSync(jsonPath, '[]');
+        console.log(` >>>\tCreado un nuevo archivo de tareas.json vacío\t<<< `);
+    } catch (err) {
+        console.error(err);
+    }
+}
 
-switch (option) {
+const action = process.argv[2];
+const firstValue = process.argv[3];
+const secondValue = process.argv[4];
+const formattedAction = upperCaseFormatter(action);
+
+console.log();
+
+switch (formattedAction) {
     case "LISTAR":
+        tasks.list();
+        break;
+    case "AGREGAR":
+        if (firstValue) {
+            const formattedSecondValue = formatStatus(secondValue);
+            tasks.add(firstValue, formattedSecondValue);
+        } else {
+            console.log(`Debe ingresar al menos un título para la tarea que desea agregar.`);
+        }
         break;
     case "BUSCAR":
-        if (!value) throw new Error("Debe ingresar un valor para realizar la búsqueda.");
-        result = tasks.search(value);
-        printOutput(result);
+        const formattedSecondValue =
+            secondValue
+                ? upperCaseFormatter(secondValue).toLowerCase()
+                : undefined;
+        firstValue
+            ? tasks.search(firstValue, formattedSecondValue)
+            : console.log(`Debe ingresar un valor para realizar la búsqueda.`);
         break;
     case "BORRAR":
+        tasks.delete;   //TODO
         break;
     case undefined:
+        console.log(`Debe ingresar una acción para ejecutar el programa.`);
         break;
     default:
+        console.log(`"${action}" no es una acción válida.`);
         break;
 }
 
-function cleanInput(input) {
+console.log();
+
+function formatStatus(status) {
+    const statusUpperCase = upperCaseFormatter(status);
+    switch (statusUpperCase) {
+        case "PENDIENTE":
+        case "TERMINADA":
+            return status;
+        case "ENPROGRESO":
+            return "EN PROGRESO";
+        default:
+            console.log(`"${status}" no corresponde con ningún valor válido. La tarea fue guardada con estado "PENDIENTE"`);
+            return "PENDIENTE";
+    }
+}
+
+function upperCaseFormatter(input = "") {
     //Le quito todo el espaciado que pueda tener
     input = input.split(" ").join("");
     //Lo convierto todo a mayúsculas
     input = input.toUpperCase();
     return input;
-}
-
-function printOutput(output) {
-    if (output.length > 0) {
-        output.forEach((element, i) => {
-            console.log(`---------------------------------------------
-    ${i+1} |  Tarea: ${element.titulo}  |  Estado: ${element.estado}`)});
-    } else {
-        console.log(`  > No se han hayado resultados :(`);
-    }
 }
--- tasks.js
+++ tasks.js
@@ -1,35 +1,78 @@
 const fs = require('fs');
-
 const jsonPath = "./tareas.json";
-
-if(! fs.existsSync(jsonPath)){
-    try {
-        fs.appendFileSync(jsonPath, '[]');
-        console.log(` >>>   Creado un nuevo archivo de tareas.json vacío    <<< `);
-    } catch (err) {
-        console.error(err);
-    }
-}
 
 let tasksArray = readJSON(jsonPath);
 
 const tasks = {
-    file: "./tareas.json",
-    list: null,
-    search: (value) => {
-        console.log(`RESULTADOS DE LA BÚSQUEDA DE "${value}":`);
-        return tasksArray.filter((task) => task.estado.toLowerCase().includes(value.toLowerCase()));
+    file: jsonPath,
+
+    list: () => printOutput(tasksArray),
+
+    add: (firstValue, secondValue) => {
+        tasksArray.push({ "titulo": firstValue, "estado": secondValue });
+        writeJSON(tasksArray);
+        console.log(`Se agregó con éxito la siguiente tarea:\n\tTítulo: ${firstValue}  |  Estado: ${secondValue}`);
     },
+
+    search: (query, by = "titulo") => {
+        foundTasks = searchTask(query, by);
+        if (foundTasks) {
+            console.log(`RESULTADOS DE LA BÚSQUEDA DE "${capitalize(by)}: ${query}" ES:`);
+            printOutput(foundTasks);
+        }
+    },
+
     delete: null,
 }
 
 module.exports = tasks;
 
-function readJSON(path) {
+function readJSON() {
     try {
-        let string = fs.readFileSync(path, { encoding: "utf-8" });
+        let string = fs.readFileSync(jsonPath, { encoding: "utf-8" });
         return JSON.parse(string);
-    } catch(error){
+    } catch (error) {
         console.error(error);
+    }
+}
+
+function writeJSON(content) {
+    const stringifiedContent = JSON.stringify(content, null, 4);
+    try {
+        fs.writeFileSync(jsonPath, stringifiedContent, { encoding: "utf-8" });
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+function searchTask(query, by) {
+    switch (by) {
+        case "titulo":
+        case "estado":
+            return tasksArray.filter((task) => task[by].toLowerCase().includes(query.toLowerCase()));
+        default:
+            console.log(`ERROR: \"${by}\" no corresponde con ningún criterio de búsqueda válido. Inténtelo nuevamente.`);
+            return;
+    }
+}
+
+function capitalize(input) {
+    const formattedInput =
+        input.replace(
+            input.charAt(0),
+            input.charAt(0).toUpperCase()
+        );
+    return formattedInput;
+}
+
+function printOutput(output) {
+    if (output.length > 0) {
+        output.map((element, i) => {
+            console.log(`-----------------------------------------------------------------------
+    ${i + 1} |  Tarea: ${element.titulo}  |  Estado: ${element.estado}`)
+        });
+        console.log(`-----------------------------------------------------------------------`);
+    } else {
+        console.log(`\t> No se han hayado resultados :(`);
     }
 }
